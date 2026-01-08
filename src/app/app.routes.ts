2b9c7050e8916161ccb238a9b17907cf38dcd475
taneralberto <taneralberto@gmail.com>
2026-01-08 19:43:32 -0400

    folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './core/auth/auth.routes';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    // component: AuthComponent,
-    loadComponent: () => import('./core/auth/test1/auth.component')
-  },
+  ...authRoutes,
 ];
---
+++ src/app/core/auth/auth.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/auth.component'),
+  },
+];
